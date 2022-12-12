9f5191d9308fd410857119d2d14a9d406ee1b4a3
Rafael Conrado <106274045+RafaelCGConrado@users.noreply.github.com>
2022-12-12 00:32:37 -0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -5,6 +5,9 @@
 typedef struct aluno ALUNO;
 typedef struct hashtable HT;
 
+//OBS: A struct aluno possui um *prox para que funcione
+//como uma lista encadeada, com a intenção de resolver
+//problemas de colisão
 struct aluno{
 
     char *nusp;
@@ -33,9 +36,14 @@ HT *cria_ht(int tamanho);
 //Cria a hash que será utilizada no index e nas senhas
 unsigned long int cria_hash(char *str);
 
+//Insere o respectivo aluno na hashtable
+void insere_ht(HT *ht, ALUNO *aluno);
+
 //Recebe as entradas e cadastra na hashtable
 void leitura_cadastro(HT *ht, int n);
 
+//realiza as buscas de fato e printa de acordo com a saída
 void busca_ht(HT *ht, char *nusp, char *senha);
 
+//Recebe as entradas 
 void busca(HT *ht, int n_senhas);
