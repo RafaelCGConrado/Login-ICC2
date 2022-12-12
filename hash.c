24cf6214d6104988609a0b0604a6a0550d726554
Rafael Conrado <106274045+RafaelCGConrado@users.noreply.github.com>
2022-12-12 00:32:14 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -18,12 +18,14 @@ char *readline(){
         }
 
         str[n_char] = c;
-        n_char ++;
+        n_char++;
+        
      }
 
     return str;
 
 }
+
 
 
 ALUNO *cria_aluno(){
@@ -61,7 +63,7 @@ unsigned long int cria_hash(char *str){
 
 void insere_ht(HT *ht, ALUNO *aluno){
 
-    //o erro está nessa funcao, certeza
+    
     unsigned long int index = (cria_hash(aluno -> nusp) % (ht -> tamanho));
 
     ALUNO *aluno_atual = ht -> alunos[index];
@@ -70,30 +72,35 @@ void insere_ht(HT *ht, ALUNO *aluno){
     if(aluno_atual == NULL){
 
         ht -> alunos[index] = aluno;
-        if(ht -> alunos[index] != NULL) printf("Cadastro efetuado com sucesso\n");
-        printf("%s", ht -> alunos[index] -> nusp);
-        if(ht -> alunos[index] != NULL) printf("ue porra\n");
+        printf("Cadastro efetuado com sucesso\n");
         return;
 
     }
-
+    
     //Caso 2: nusp são iguais
-    if(strcmp(ht -> alunos[index] -> nusp, aluno_atual -> nusp) == 0) printf("NUSP ja cadastrado\n");
+    if(strcmp(aluno_atual -> nusp, aluno -> nusp) == 0){
+        printf("NUSP ja cadastrado\n");
+        return;
 
-    //Caso 3: Colisão (É preciso lidar usando listas)
-    else{
+    }
+    
+
+   // Caso 3: Colisão (Resolvendp com listas encadeadas)
+       
         ALUNO *p = aluno_atual;
-        ALUNO *aux = aluno -> prox;
+        ALUNO *anterior;
+
 
         while(p != NULL){
             
+            anterior = p;
             p = p -> prox;
+
         }
-
-        p = aux;
-    }
-
-
+        
+        anterior -> prox = aluno;
+        printf("Cadastro efetuado com sucesso\n");
+        return;
 
 }
 
@@ -107,11 +114,10 @@ void leitura_cadastro(HT *ht, int n){
         nusp = readline();
         senha = readline();
         unsigned long int senha_hash = cria_hash(senha);
+        
 
         aluno_atual -> nusp = nusp;
         aluno_atual -> senha = senha_hash;
-
-        printf("%s\n", aluno_atual -> nusp);
 
 
         for(int j = 0; j < 4; j++){
@@ -134,76 +140,68 @@ void busca_ht(HT *ht, char *nusp, char *senha){
     unsigned long int senha_hs = cria_hash(senha);
 
     ALUNO *aluno_atual = ht -> alunos[index];
-    //o problema esta aqui, certeza!!!!!!
-    if(aluno_atual != NULL) printf("Achei\n");
-
-
     
+    if(aluno_atual != NULL){
 
-    //NUSP existe e ocupa o "topo" da lista
-    if(strcmp(aluno_atual -> nusp, nusp) == 0){
+        //Primeiro caso: Existe e é o aluno buscado
+        if(strcmp(aluno_atual -> nusp, nusp) == 0){
+
+            if(senha_hs == aluno_atual -> senha){
+
+                printf("Notas: ");
+                printf("P1=%.1lf, P2=%.1lf, ", aluno_atual -> notas[0], aluno_atual -> notas[1]);
+                printf("T1=%.1lf, T2=%.1lf\n", aluno_atual -> notas[2], aluno_atual -> notas[3]);
+                return;
+
+            }
+
+            printf("Senha incorreta para o NUSP digitado\n");
+            return;
+        }
+
+        //Segundo caso: Existe, mas ainda não é o buscado (colisao)
+        ALUNO *p = aluno_atual -> prox;
         
-        if(senha_hs == aluno_atual -> senha){
-            
-            printf("P1:%.1lf, ", aluno_atual -> notas[0]);
-            printf("P2:%.1lf, ", aluno_atual -> notas[1]);
-            printf("T1:%.1lf, ", aluno_atual -> notas[2]);
-            printf("T2:%.1lf\n", aluno_atual -> notas[3]);
+
+        while(p != NULL){
+            if(strcmp(p -> nusp, nusp) == 0){
+                
+                if(senha_hs == p -> senha){
+
+                    printf("Notas: ");
+                    printf("P1=%.1lf, P2=%.1lf, ", p -> notas[0], p -> notas[1]);
+                    printf("T1=%.1lf, T2=%.1lf\n", p -> notas[2], p -> notas[3]);
+                    return;
+
+                }
+                printf("Senha incorreta para o NUSP digitado\n");
+                return;
+
+            }
+
+            //Ainda não é o aluno procurado. Avançamos para
+            //o próximo aluno da lista
+            p = p -> prox;
 
         }
 
-        else printf("Senha incorreta\n");
 
-        
-    
+    }
 
-    //     else printf("Senha incorreta para o NUSP digitado\n");
-
-   }
-
-    // //NUSP existe mas não é o do "topo" da lista (colisão)
-
-    
-    // aluno_buscado = aluno_buscado -> prox;
-    // while(aluno_buscado != NULL){
-        
-        
-    //     if(strcmp(aluno_buscado -> nusp, nusp) == 0){
-
-    //         if(senha_hs == aluno_buscado -> senha){
-                
-    //             printf("P1:%1.lf, ", aluno_buscado -> notas[0]);
-    //             printf("P2:%1.lf, ", aluno_buscado -> notas[1]);
-    //             printf("T1:%1.lf, ", aluno_buscado -> notas[2]);
-    //             printf("T2:%1.lf\n", aluno_buscado -> notas[3]);
-
-    //         }
-
-    //         else printf("Senha incorreta para o NUSP digitado\n");
-
-    //     }
-    //     aluno_buscado = aluno_buscado -> prox;
-        
-    // }
-
-    // //NUSP não existe
-    // if(aluno_buscado == NULL){
-
-    //     printf("NUSP invalido\n");
-
-
-    // }
+    //Ultimo caso: Não foi registrado
+    printf("NUSP invalido\n");
+    return;
 
 }
 
 void busca(HT *ht, int n_senhas){
 
     char *nusp;
-    char *senha;
+    char senha[50];
 
     for(int i = 0; i < n_senhas; i++){
         nusp = readline();
-        senha = readline();
+        scanf("%s", senha);
 
         busca_ht(ht, nusp, senha);
     }
